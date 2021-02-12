cdcb2ada223e38d100c6297a1f9edaf70dad2919
Rafał Nowak <rafal.robi.nowak@gmail.com>
2021-02-12 16:23:27 +0100

    W tabeli 'users' została dodana rola użytkownika

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->enum('role', ['admin', 'author', 'subcriber'])->default('author');
+            
             $table->rememberToken();
             $table->timestamps();
         });
