d471960f7478f9ba1e9d4486525d70a768eafa5b
Rafał Nowak <rafal.robi.nowak@gmail.com>
2021-02-13 12:09:04 +0100

    Dodatkowa klasa z metodami do walidacji danych z formularza dla postu

---
+++ app/Http/Requests/PostFormRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostFormRequest extends FormRequest
+{
+  //Sprawdzenie czy użytkownik ma uprawnienia do wywołania tego sprawdzania
+  public function authorize(){
+    if($this->user()->can_post()){
+      return true;
+    }
+    return false;
+  }
+
+  //sprawdzenie poprawności wprowadzonych danych
+  //zwraca tablicę z reguałmi które powinnien przejść wskazany input z formularza
+  public function rules(){
+    return [
+      'title' => 'required|unique:posts|max:255',
+      'title' => array('regex:/^[A-Za-z0-9]+$/'),
+      'body' => 'required',
+    ];
+  }
+}
