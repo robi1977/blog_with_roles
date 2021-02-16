55c554e31df5d2f513210765baabbf63ef027e07
Rafal Nowak <rafal.robi.nowak@gmail.com>
2021-02-16 11:03:22 +0100

    Zmodyfikowano podstawowe widoki: ogólny layout -> app i widok 'home'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,43 @@
 @extends('layouts.app')
 
+@section('title')
+  {{$title}}
+@endsection
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+  @if (!$posts->count())
+    Brak postów do wyświetlenia
+  @else
+    <div class="">
+      @foreach ($posts as $post)
+        <div class="list-group">
+          <div class="list-group-item">
+            <h3>
+              <a href="{{ url('/'.$post->slug) }}">
+                {{ $post->title}}
+              </a>
+              <!--TODO: przemyśleć skrócenie poniższych 2ch if'ów -->
+              @if (!Auth::guest() && ($post->author_id ==Auth::user()->id || Auth::user()->is_admin()))
+                @if ($post->active == 1)
+                  <button class="btn" style="float: right">
+                    <a href="{{ url('edit/'.$post->slug) }}">Edit post</a>
+                  </button>                    
+                @else
+                <button class="btn" style="float: right">
+                  <a href="{{ url('edit/'.$post->slug) }}">Edit draft</a>
+                </button> 
+                @endif
+              @endif
+            </h3>
+            <p>{{ $post->created_at->format('M d,Y \a\t h:i a')}} by <a href={{ url('/user/'.$post->author_id)}}>{{ $post->author->name }}</a></p>
+          </div>
+          <div class="list-group-item">
+            <article>
+              {!! Str::limit($post->body, 1500, '...<a href='.url("/".$post->slug).'>Read more</a>') !!}
+            </article>
+          </div>
         </div>
+      @endforeach
+      {!! $posts->render() !!}
     </div>
-</div>
+  @endif
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,64 +20,95 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-                            
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle Navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="https://www.flowkl.com">Flowkl</a>
+      </div>
+      <div class="collapse navbar-collapsed" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li>
+            <a href="{{ url('/')}}">Home</a>
+          </li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          @if(Auth::guest())
+            <li>
+              <a href="{{ url('/auth/login') }}">Login</a>
+            </li>
+            <li>
+              <a href="{{ url('/auth/register') }}">Register</a>
+            </li>
+          @else 
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }}<span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                @if(Auth::user()->can_post())
+                  <li>
+                    <a href="{{ url('new-post') }}">Add new post</a>
+                  </li>
+                  <li>
+                    <a href="{{ url('/user/'.Auth::id().'/posts') }}">My posts</a>
+                  </li>
+                @endif
+                  <li>
+                    <a href="{{ url('/user/'.Auth::id()) }}">My profile</a>
+                  </li>
+                  <li>
+                    <a href="{{ url('/logout') }}">Logout</a>
+                  </li>
+              </ul>
+            </li>              
+          @endif
+        </ul>
+      </div>
     </div>
+  </nav>
+  <div class="container">
+    @if(Session::has('message'))
+      <div class="flash alert-info">
+        <p class="panel-body">
+          {{ Session::get('message') }}
+        </p>
+      </div>
+    @endif
+    @if($errors->any())
+      <div class="flash alert-danger">
+        <ul class="panel-body">
+          @foreach ($errors->all() as $error)
+            <li>
+              {{ $error }}
+            </li>    
+          @endforeach
+        </ul>
+      </div>
+    @endif
+    <div class="row">
+      <div class="col-md-10 col-md-offset-1">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h2>@yield('title')</h2>
+            @yield('title-meta')
+          </div>
+          <div class="panel-body">
+            @yield('content')
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-10 col-md-offset-1">
+        <p>Copyright <a href="https://www.flowkl.com">Flowkl</a></p>
+      </div>
+    </div>
+  </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
